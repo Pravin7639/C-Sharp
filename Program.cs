d5d2e145f5c64ffaa5bb6654554289ea8b53b144
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-03 23:01:20 +0530

    06_Nullable Types

--- Program.cs
+++ Program.cs
@@ -4,74 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _5_TypeConversion
+namespace _6_NullableTypes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 10, j = 10;
-            Console.WriteLine(i + j); // 20
-
-            // int => string
-            string a = i.ToString(); // int -> string
-            string b = j.ToString();
-            Console.WriteLine(a + b); // 1010
-
-            byte b1 = 10;
-            short s1 = b1; // byte -> short
-            Console.WriteLine(s1); // 10
-
-            float f1 = s1; // short -> float
-            Console.WriteLine(f1); // 10
-
-            // int i1 = f1; // float -> int
-            int i1 = (int)f1; // float -> int
-            Console.WriteLine(i1);
-
-            f1 = 25.55f;
-            i1 = (int)f1;
-            Console.WriteLine(i1); // 25.55
-
-            string s = "2000";
-            // string s = "ABCD";
-            // i1 = s; // string -> int
-            i1 = Convert.ToInt32(s);
-            Console.WriteLine(i1); // 100
-
-            s = "true";
-            bool bb = Convert.ToBoolean(s); // string -> bool
-            Console.WriteLine(bb);
+            string Name = "Pravin";
+            Console.WriteLine(Name);
+            Name = null;
+            Console.WriteLine(Name);
 
 
-            s = "777";
-            // Parse()
-            // Parse() method throws error when string is not in correct format
-            i1 = int.Parse(s); // string -> int
-            Console.WriteLine(i1); // 777
 
-            //s = "XYZ";
-            //i1 = int.Parse(s); // runtime error
-            //Console.WriteLine(i1);
+            //////////////  convert int value to null
+            int? age = null;
 
-            // TryParse() -> it handles the runtime error
-            // convert and return conversion status along with converted value
-            // in case of runtime error it returns false, default value as output
-            // default value of bool is false
-            // default value of int is 0
-            // default value of string is null
+            // Console.WriteLine(age);
 
-            s = "2000";
-            bb = int.TryParse(s, out i1);
-            Console.WriteLine($"Status : {bb} Result : {i1}");
+            bool? isDelivered = null;
 
-            s = "XYZ";
-            bb = int.TryParse(s, out i1);
-            Console.WriteLine($"Status : {bb} Result : {i1}"); // false, 0
+            DateTime? DOB = null;
 
-            decimal x = 10.52m;
-            long l = (long)x; // decimal -> long
-            Console.WriteLine(l); // 10
+
+            //int? a = 10;
+            //int b = (int)a;
+            //Console.WriteLine($" a : {a}  b : {b}");
+
+
+
+            ////////////   to convert nullable type to non-nullable type
+            ///
+
+            int? a = null;
+            int b = a ?? 0;
+            Console.WriteLine($" a : {a}  b : {b}");
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///
+
+            string email = "pravinmane9276@gmail.com";
+            string emailInUpper = email.ToUpper();
+            Console.WriteLine($"email : {email}\nemailInUpper : {emailInUpper}");
+
+            email = null;
+            emailInUpper = email?.ToUpper();
+            Console.WriteLine($"email : {email}\nemailInUpper : {emailInUpper}");
+
+
+
+
 
             Console.ReadLine();
         }
