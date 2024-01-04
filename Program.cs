553d28899230adf522eb89bd65566b63983a9fae
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-04 22:44:10 +0530

    10_Array

--- Program.cs
+++ Program.cs
@@ -4,311 +4,158 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _8_LoopingStatement
+namespace _10_Array
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            #region While Loop
-            //int i = 1;
-            //while(i <= 10){
-            //    Console.Write($"{i} ");
-            //    i++;
-            //}
-            //Console.WriteLine();
-            //int j = 1;
-            //while (j <= 10)
-            //{
-            //    Console.Write("* ");
-            //    j++;
-            //}
-
-            //int k = 1;
-            //while (k <= 0)
-            //{
-            //    Console.Write($"{k} ");
-            //    k++;
-            //}
 
 
+            int[] fees = new int[3];
+            fees[0] = 20000;
+            fees[1] = 15000;
+            fees[2] = 25000;
+            for (int i = 0; i < fees.Length; i++)
+            {
+                Console.Write($"Number of Items : {fees[i]} ");
+            }
+
+            Console.WriteLine();
+
+            string[] name = new string[] { "Pravin", "Pawan", "Ram", "Ravan" };
+
+            Console.Write($"Length of string :{name.Length}");
+
+            for (int j = 0; j < name.Length; j++)
+            {
+                Console.WriteLine($"names : {name[j]}");
+            }
 
 
-            #endregion While Loop
-
-
-
-            #region Do While Loop
-
-            //int i = 1;
-            //do
-            //{
-            //    Console.Write($"{i} ");
-            //    i++;
-            //}
-            //while (i <= 10);
-
-            //Console.WriteLine();
-
-
-            //int j = 1;
-            //do
-            //{
-            //    Console.Write($"{j} ");
-            //    j++;
-            //}
-            //while (j <= 0);
-
-            #endregion Do While Loop
-
-
-
-            #region For Loop
-
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    Console.Write("* ");
-            //}
-
-            //Console.WriteLine();
-
-            //for (int j = 1; j <= 10; j++)
-            //{
-            //    Console.Write(j+ " ");
-            //}
-
-            #endregion For Loop
-
-
-
-            #region Square Pattern
-            //for(int i = 1; i <= 5; i++)
-            //{
-            //    for (int j = 1; j <= 5; j++)
-            //    {
-            //        Console.Write("* ");
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            #endregion Square  Pattern
+            //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 
 
 
-            #region Hollow Square Pattern
-            // int n=5;
-            //for (int i = 1; i <= n; i++)
-            //{
-            //    for (int j = 1; j <= n; j++)
-            //    {
-            //        if (i == 1 && j <= n || j == 1 && i <= n || i==n && j<=n || j==n && i<=n)
-            //        {
-            //            Console.Write("* ");
-            //        }
-            //        else
-            //        {
-            //            Console.Write("  ");
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //}
 
-            #endregion Hollow Square Pattern
-
-
-
-            #region Right Angle
-            //for (int i = 1; i <= 5; i++)
-            //{
-            //    for(int j = 1; j <= i; j++)
-            //    {
-            //        Console.Write("* ");
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            ////////////////////////////////////////////////////////////////////////
-            ////////////////////////////////////////////////////////////////////
+            ///////////////////Creating Array Of Empty 
             ///
 
-            //for(int i = 1; i <= 5; i++)
-            //{
-            //    for(int j = 1; j <= i; j++)
-            //    {
-            //        Console.Write(j + " ");
-            //    }
-            //    Console.WriteLine();
-            //}
+            string[] email = null;
+            email = new string[] { " p@p.com", "args@a.com", "k@k.com", "m@m.com", "s@s.com" };
+
+            Console.WriteLine(email.Length);
+
+
+
+            //////////////////// Reversing Array
+            ///
+
+            int[] num = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("orignal array");
+
+            for (int k = num.Length - 1; k >= 0; k--)
+            {
+                Console.Write(num[k] + " ");
+            }
+
+
+            Console.WriteLine();
+
+            ////////////find highest numbe from array
+            ///
+
+            int[] n = new int[] { 12, 21, 32, 23, 43, 34, 45, 45, 56, 67, 99 };
+
+            int max = n[0];
+            for (int l = 0; l < n.Length; l++)
+            {
+                if (max < n[l])
+                {
+                    max = n[l];
+                }
+
+            }
+            Console.WriteLine(max);
+
+
+
+
+            ///////////////////// Reverse Input String 
+            ///
+
+            Console.WriteLine("Please enter a name");
+            string nam = Console.ReadLine();
+
+            string reverse = string.Empty;
+
+            for (int o = nam.Length - 1; o >= 0; o--)
+            {
+                reverse += nam[o];
+            }
+
+            Console.WriteLine(reverse);
+
+
+
+            //////////////////////// Reverse Words IN Sentence
+            ///
+
+            Console.WriteLine("Please Enter A Sentence");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(new char[] { ' ' });
+
+            Console.WriteLine("output : ");
+
+            for (int p = words.Length - 1; p >= 0; p--)
+            {
+                Console.Write($"{words[p]} ");
+            }
+
+            Console.WriteLine();
+
+            ////////////////////////////////Reversing Sentence
+            ///
+
+            Console.WriteLine("Please Enter A Sentence");
+            string sentences = Console.ReadLine();
+
+            string ReversedSentences = string.Empty;
+            for (int q = sentences.Length - 1; q >= 0; q--)
+            {
+                ReversedSentences += sentences[q];
+
+            }
+
+            Console.WriteLine(ReversedSentences);
+
+
+
+            //////// sorting of array
+            ///
+
+
+
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda", "BMW", "Volvo" };
+            Array.Sort(cars);
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
 
 
 
 
 
-            #endregion Right Angle
-
-
-
-            #region Right Angle Reverse
-            //for (int i = 5; i >= 1; i--)
-            //{
-            //    for(int j = 1; j < i; j++)
-            //    {
-            //        Console.Write("  ");
-            //    }
-
-            //    for(int k = 5; k >= i; k--)
-            //    {
-            //        Console.Write("* ");
-            //    }
-            //    Console.WriteLine();
-        
-
-        #endregion Right Angle Reverse
-
-
-
-        #region Reverse Right Angled Triangled
-
-        //for (int i = 5; i >= 1; i--)
-        //{
-        //    for (int j = 1; j <= i; j++)
-        //    {
-        //        Console.Write(j + " ");
-        //    }
-        //    Console.WriteLine();
-        //}
-
-
-        /////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////
-        ///
-
-        //for (int i = 5; i >= 1; i--)
-        //{
-        //    for (int j = 1; j <= i; j++)
-        //    {
-        //        Console.Write("* ");
-        //    }
-        //    Console.WriteLine();
-        //}
-
-        #endregion Reverse Right Angled Triangled
-
-
-
-        #region Plus Pattern
-        //for (int i = 1; i <= 5; i++)
-        //{
-        //    for(int j = 1; j <= 5; j++)
-        //    {
-        //        if(j == 3 && i <=5   || i==3 && j <= 5)
-        //        {
-        //            Console.Write("* ");
-        //        }
-        //        else
-        //        {
-        //            Console.Write("  ");
-        //        }
-        //    }
-        //    Console.WriteLine();
-        //}
-
-        #endregion Plus Pattern
-
-
-
-        #region Random Pattern
-
-
-        //for (int i = 1; i <= 5; i++)
-        //{
-        //    for (int j = 1; j <= 5; j++)
-        //    {
-        //        if (j == 3 && i <= 5 || i == 3 && j <= 5 || i==2 && j==2 ||i==4 && j==4 || i==2 && j==4 || i==4 && j==2)
-        //        {
-        //            Console.Write("* ");
-        //        }
-        //        else
-        //        {
-        //            Console.Write("  ");
-        //        }
-        //    }
-        //    Console.WriteLine();
-        //}
-
-
-        #endregion Random Pattern
-
-
-        #region ButterFly Pattern
-
-
-        //        //for (int i = 1; i <= 11; i++)
-        //        //{
-        //        //    for (int j = 1; j <= 11; j++)
-        //        //    {
-        //        //        if (j == 1 && i <= 11 || j == 2 && i+1 <= 10 || j == 3 && i + 2 <= 9 || j == 4 && i + 3 <= 8 || j == 5 && i + 4 <= 7 || i == 6 && j == 6
-        //        //            || j == 11 && i <= 11 || j == 10 && i + 1 <= 10 || j == 9 && i + 2 <= 9 || j == 8 && i + 3 <= 8 || j == 7 && i + 4 <= 7)
-        //        //        {
-        //        //            Console.WriteLine("* ");
-        //        //        }
-        //        //        else
-        //        //        {
-        //        //            Console.WriteLine("  ");
-
-        //        //        }
-        //        //       // Console.WriteLine();
-        //        //    }
-        //    }
-
-
-
-        //    int i, j, n = 5;
-        //            for (i = 1; i <= n; i++)
-        //            {
-        //                for (j = 1; j <= i; j++)
-        //                {
-        //                    Console.Write("*");
-        //                }
-        //                for (j = 1; j <= (n - i); j++)
-        //            {
-        //                Console.Write("  ");
-        //            }
-        //              for (j = 1; j <= i; j++)
-        //             {
-        //               Console.Write("*");
-        //            }
-        //             Console.WriteLine();
-        //                }
-
-        //            for (i = n - 1; i >= 1; i--)
-        //            {
-        //    for (j = 1; j <= i; j++)
-        //    {
-        //        Console.Write("*");
-        //    }
-        //    for (j = 1; j <= (n - i); j++)
-        //    {
-        //        Console.Write("  ");
-        //    }
-        //    for (j = 1; j <= i; j++)
-        //    {
-        //        Console.Write("*");
-        //    }
-        //    Console.WriteLine();
-        //}
-        //  }
-        #endregion ButterFly Pattern
 
 
 
 
 
-        Console.ReadLine();
-
+            Console.ReadLine();
         }
     }
 }
-
