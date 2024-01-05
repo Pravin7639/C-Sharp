32b4bfa5d886ed1fde119a3003449b30acf01e6f
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-05 23:22:08 +0530

    13_Method Parameter

--- Program.cs
+++ Program.cs
@@ -4,142 +4,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _12_MethodsDemo
+namespace _13_MethodParameter
 {
     class Program
     {
         static void Main(string[] args)
         {
 
+            int a = 10;
+            PrintA(a);
+            Console.WriteLine(a);   ///  10
+
+            //int m = 200;
+            //PrintA(m);
+            //Console.WriteLine(m);   ///   200
+
+            /////////////////////////////////////////////////////////////////////////
+
+            a = 10;   ////first we have to assign the value
+
+            PrintB(ref a);
+            Console.WriteLine(a);    /////   100
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+
+            int x = 40, y = 20, add, sub, mul, div;
+            PrintD(x, y, out add, out sub, out mul, out div);
+            Console.WriteLine($"Addition :{x} + {y} = {x + y}");
+            Console.WriteLine($"subtraction :{x} - {y} = {x - y}");
+            Console.WriteLine($"Multiplication :{x} * {y} = {x * y}");
+            Console.WriteLine($"Devision :{x} / {y} = {x / y}");
+
+            Console.WriteLine();
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+            ///
+
+            int[] nums = { 10, 20, 30 };
+            Print(nums);
+            Print(new int[] { 10, 20, 30 });
+            Print(10, 20, 30);
+
+            Console.WriteLine();
 
 
-
-            //  PrintA();
-
-            //  PrintB("Pravin");
-            //  string b = "PRAVIN";
-            //  PrintB(b);
-
-            //  PrintC("Pravin", "Mane");
-            //  string f = "PRAVIN", l = "MANE";
-            //  PrintC(f, l);
-
-            //string result=  PrintD("Pravin", "Mane");
-            //  Console.WriteLine($"PrintD result : FULLNAME : {result}");
-
-            //  PrintF(true);
-            //  PrintF(false);
-
-            //  int[] nums = new int[] { 12, 12, 12, 12, 12, 12, 12 };
-            //  PrintG(nums);
-
-
-            Console.WriteLine("Please enter a number");
-            int m = int.Parse(Console.ReadLine());
-
-            for (int i = 2; i <= m; i++)
-            {
-                if (isPrime(i))
-                {
-                    Console.Write(i + " ");
-
-                }
-
-            }
-
+            int[] num = null;
+            Print(num);
+            Print(null);
+            Print();
 
             Console.ReadLine();
         }
 
-        //  //// 1... Method without return and without Parameter
-
-        //    static void PrintA()
-        //    {
-        //        Console.WriteLine($"PrintA() : Method called");
-        //    }
-
-
-        //    ///// 2... Method without return and with Parameter
-
-        //static void PrintB(string name)
-        //    {
-        //        Console.WriteLine($"PrintB() : Hello, {name}");
-        //    }
-
-
-        /////// 3.....Method without return and Multiple Parameter
-        /////
-
-        //    static void PrintC (string fn, string ln)
-        //    {
-        //        Console.WriteLine($"PrintC : Fullname : {fn} {ln} ");
-        //    }
 
 
 
 
-        //    ///////4.....Method with return and with Parameter
-
-
-        //    static string PrintD (string fn, string ln)
-        //    {
-        //        return fn + " " + ln;
-        //    }
-
-        ////////// 5....Return statement in void....
-        /////
-
-        //    static void PrintF(bool isConfirmed)
-        //    {
-        //        if (!isConfirmed)
-        //        {
-        //            return;
-        //        }
-        //        Console.WriteLine($"PrintE ({isConfirmed}) : Seesion Allowed ");
-        //    }
 
 
 
-        //    ///////////// 6.... Sum of Array
-        //    ///
-        //     static void PrintG (int[] nums)
-        //    {
-        //        if(nums !=null && nums.Length > 0)
-        //        {
-        //            int sum = 0;
-        //            for (int i = 0; i < nums.Length; i++)
-        //            {
-        //                sum += nums[i];
-        //            }
-        //            Console.WriteLine($" PrintG : Sum Of Array :{sum} ");
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("No Number To Add");
-        //        }
-        //    }
 
 
 
-        static bool isPrime(int n)
+
+        //Pass By Value
+
+        static void PrintA(int a)
         {
-            int count = 1;
-            for (int i = 2; i <= n; i++)
-            {
-                if (n % i == 0)
-                {
-                    count++;
-                }
-            }
-            if (count == 2)
-            {
-                return true;
+            a = 100;
+        }
 
+        //Ref KeyWord
+
+        static void PrintB(ref int a)
+        {
+            ///a = 100;    /// it should work in this condition also but value should printed which is assigned in the Main Method
+            a = 100;
+        }
+
+        static void PrintD(int a, int b, out int add, out int sub, out int mul, out int div)
+        {
+            add = a + b;
+            sub = a - b;
+            mul = a * b;
+            div = a / b;
+        }
+
+
+
+        static void Print(params int[] numbers)
+        {
+            if (numbers != null && numbers.Length > 0)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                Console.WriteLine($"Sum of Array : {sum}");
             }
             else
             {
-                return false;
+                Console.WriteLine("EMPTY ARRAY");
             }
+
         }
 
 
