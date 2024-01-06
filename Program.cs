3d4b631d7940c86a3dc10eb8fbd454624526a777
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-06 23:32:34 +0530

    15_Class

--- Program.cs
+++ Program.cs
@@ -3,75 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
-namespace _14_OptionalMethodParameter
+namespace _15_Class
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Student s = new Student();
+            s.RollNumber = 1;
+            s.Name = "Pravin";
+            s.City = "Pune";
+            s.Details();
 
-            add(10, 20);
-
-            //default value
-
-            add1(10);
-
-            add2();
-
-
-            add3(10, 20);
-            add3(10);
-
-
-            add4(10, 20);
-            add4();
+            Student s2 = new Student();
+            s2.RollNumber = 7;
+            s2.Name = "Ram";
+            s2.City = "Ayodhya";
+            s2.Details();
 
 
+            s2.RollNumber = 9;
+            s2.Name = "Pawan";
+            s2.City = "Pune";
+            s2.Details();
+            ////////////////////////////////////////////////////////////////////////////////////////
+
+            Circle c1 = new Circle();
+            c1.Radius = 5;
+            c1.Area();
+
+            c1.Radius = 10;
+            c1.Area();
 
 
 
             Console.ReadLine();
         }
-
-
-        static void add(int a, int b)
-        {
-            Console.WriteLine($"Addition : {a} + {b} = {a + b}");
-        }
-
-
-
-        /// Giving default value 
-        /// always give default value to last parameter
-        /// 
-        static void add1(int a, int b = 5)
-        {
-            Console.WriteLine($"Addition : {a} + {b} = {a + b}");
-
-        }
-
-
-        static void add2(int a = 10, int b = 20)
-        {
-            Console.WriteLine($"Addition : {a} + {b} ={a + b}");
-        }
-
-
-
-        /// using optional Parameter
-        static void add3(int a, [Optional] int b)
-        {
-            Console.WriteLine($"Addition : {a} + {b} ={a + b} ");
-        }
-
-
-        static void add4([Optional] int a, [Optional] int b)
-        {
-            Console.WriteLine($"Addition : {a} + {b} ={a + b}");
-        }
-
-
     }
+
+    public class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+
+        public void Details()
+        {
+            Console.WriteLine($"Roll Number : {RollNumber} Name : {Name} City : {City}");
+        }
+    }
+
+    public class Circle
+    {
+        public int Radius { get; set; }
+        public static double PI = 3.143;
+
+        public void Area()
+        {
+            Console.WriteLine($"Area of Circle : {Radius * Radius * PI}");
+        }
+    }
+
 }
