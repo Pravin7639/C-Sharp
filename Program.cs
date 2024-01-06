5c3ccdaab5bb6cb695531b0320a4367b18bfc76c
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-06 23:35:32 +0530

    16_Boxing UnBoxing

--- Program.cs
+++ Program.cs
@@ -4,66 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _15_Class
+namespace _16_BoxingUnBoxing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Student s = new Student();
-            s.RollNumber = 1;
-            s.Name = "Pravin";
-            s.City = "Pune";
-            s.Details();
+            int i = 10;
+            Console.WriteLine($"i  : {i} ");
 
-            Student s2 = new Student();
-            s2.RollNumber = 7;
-            s2.Name = "Ram";
-            s2.City = "Ayodhya";
-            s2.Details();
+            object o = i;    
+            Console.WriteLine($"o  : {o} ");
+            /////////////////     value type to reference type ---> Boxing............
 
 
-            s2.RollNumber = 9;
-            s2.Name = "Pawan";
-            s2.City = "Pune";
-            s2.Details();
-            ////////////////////////////////////////////////////////////////////////////////////////
+            int j = (int)o;
+            Console.WriteLine($"j  : {j}");    
+            ///////////    Reference type to Value type ---> UnBoxing.............
 
-            Circle c1 = new Circle();
-            c1.Radius = 5;
-            c1.Area();
-
-            c1.Radius = 10;
-            c1.Area();
 
 
 
             Console.ReadLine();
         }
     }
-
-    public class Student
-    {
-        public int RollNumber { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-
-
-        public void Details()
-        {
-            Console.WriteLine($"Roll Number : {RollNumber} Name : {Name} City : {City}");
-        }
-    }
-
-    public class Circle
-    {
-        public int Radius { get; set; }
-        public static double PI = 3.143;
-
-        public void Area()
-        {
-            Console.WriteLine($"Area of Circle : {Radius * Radius * PI}");
-        }
-    }
-
 }
