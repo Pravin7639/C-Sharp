78c1d1747d50299c87255cee795b1ed9016ec9d5
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-06 23:36:16 +0530

    17_Static Demo

--- Program.cs
+++ Program.cs
@@ -4,28 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _16_BoxingUnBoxing
+namespace _17_StaticDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 10;
-            Console.WriteLine($"i  : {i} ");
 
-            object o = i;    
-            Console.WriteLine($"o  : {o} ");
-            /////////////////     value type to reference type ---> Boxing............
-
-
-            int j = (int)o;
-            Console.WriteLine($"j  : {j}");    
-            ///////////    Reference type to Value type ---> UnBoxing.............
+            Student s1 = new Student();
+            s1.RollNumber = 1;
+            s1.Name = "Pravin";
+            s1.City = "Pune";
+            s1.Details();
 
 
 
+            Student s2 = new Student(2, "Pawan", "Pandharpur");
+            s2.Details();
+
+
+            //we can change the value of static member
+            Student.CourseName = "C#";
+
+            s2.Details();
 
             Console.ReadLine();
         }
     }
+
+    public class Student
+    {
+        public int RollNumber { get; set; }   // Instance of Class
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public static string CourseName = "DOT NET";
+
+        public Student()   // Constructor Without Parameter
+        {
+
+        }
+
+
+        public Student(int rn, string name, string city) // Constructor With Parameter
+        {
+            this.RollNumber = rn;
+            this.Name = name;
+            this.City = city;
+
+        }
+
+        public void Details()
+        {
+            Console.WriteLine($"Roll Number : {RollNumber} Name : {Name} City : {City} " +
+                $"Course Name : {CourseName} ");
+        }
+
+    }
+
+
 }
