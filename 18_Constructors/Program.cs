609c79acedb3862550211dde8f552a94f17dca18
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-07 11:58:43 +0530

    18_Constructor

---
+++ 18_Constructors/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student(1, "Pravin", "Mane");   //// OBJECT INITILIZER SYNTAX   
+            s1.Details();
+
+            Student s2 = new Student(2, "Pawan", "Mane");
+            s2.Details();
+
+
+            Student s3 = new Student()
+            {
+                RollNumber = 9,
+                FirstName = "Pawan",
+                LastName = "mane"
+            };
+            s3.Details();
+
+
+
+           Student s4 = new Student();
+            s4.Details();
+
+
+
+
+
+            /////////////////////////COPY CONSTRUCTOR//////////////////////////////////////
+            ///
+            Student s5 = s1;
+            s5.Details();
+
+
+            Student s6 = new Student();
+            s6.RollNumber = s1.RollNumber;
+            s6.FirstName = s1.FirstName;
+            s6.LastName = s1.LastName;
+            s6.Details();
+
+            ///using copy constructor
+            ///
+            Student s7 = new Student(s2);
+            s7.Details();
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 18_Constructors/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructors
+{
+   public class Student
+    {
+        public int RollNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public Student()  /// Constructor without Parameter
+        {
+
+        }
+
+
+        public Student(int rn, string fn, string ln)  /// Constructor with Parameter
+        {
+            this.RollNumber = rn;
+            this.FirstName = fn;
+            this.LastName = ln;
+
+        }
+
+        public void Details()
+        {
+            Console.WriteLine($"Roll Number : {RollNumber} Name : {FirstName} {LastName}");
+        }
+
+
+
+
+
+
+
+
+        ///////////////COPY CONSTRUCTOR//////////////////////////
+        ///
+
+        public Student( Student s)
+        {
+            this.RollNumber = s.RollNumber;
+            this.FirstName = s.FirstName;
+            this.LastName = s.LastName;
+        }
+
+
+
+
+    }
+}
