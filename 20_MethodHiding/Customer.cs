1b826f2dee33deb2dcde4a0a5ad16a2411423c53
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-07 12:01:43 +0530

    20_Method Hiding

---
+++ 20_MethodHiding/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_MethodHiding
+{
+   public class Customer
+    {
+        public void Print()
+        {
+            Console.WriteLine("Customer Print() Method Called");
+        }
+
+        public void PrintA()
+        {
+            Console.WriteLine("Customer PrintA() Method Called");
+        }
+    }
+
+    public class SilverCustomer : Customer
+    {
+
+        //using NEW keyword we can hide the method
+
+        new public void Print()
+        {
+            Console.WriteLine("Silver Customer Print() Method Called");
+        }
+
+        public void PrintB()
+        {
+            Console.WriteLine("Silver Customer Print() Method Called");
+        }
+
+    }
+
+}
---
+++ 20_MethodHiding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_MethodHiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.Print();
+            c1.PrintA();
+
+
+            Console.WriteLine("--------------------------------------------");
+
+            SilverCustomer sc = new SilverCustomer();
+            sc.Print();
+            sc.PrintA();
+            sc.PrintB();
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
