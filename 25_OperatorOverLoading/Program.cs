3ae16be6a7588e079ea69f1c66153fd3edea45a9
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-08 17:44:09 +0530

    25_Operator Overloading

---
+++ 25_OperatorOverLoading/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_OperatorOverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+             int j = 10;
+            //int j = 20;
+            if (i == j)
+            {
+                Console.WriteLine("EQUAL");
+            }
+            else
+            {
+                Console.WriteLine("NOT EQUAL");
+            }
+
+
+            string str1 = "Pawan";
+             string str2 = "Pawan";
+            //string s2 = "Pravin";
+            if (str1 == str2)
+            {
+                Console.WriteLine("EQUAL");
+            }
+            else
+            {
+                Console.WriteLine("NOT EQUAL");
+            }
+
+
+            Student s1 = new Student(1, "Pravin", "Mane");
+            //Student s2 = s1;
+            Student s2 = new Student(1, "Pravin", "Mane");
+            if (s1 == s2)
+            {
+                Console.WriteLine("EQUAL");
+            }
+            else
+            {
+                Console.WriteLine("NOT EQUAL");
+            }
+
+
+
+            Student s3 = s1 + s2;
+            Console.WriteLine($" {s3.RollNumber}  :  {s3.FirstName}  :  {s3.LastName} ");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 25_OperatorOverLoading/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_OperatorOverLoading
+{
+    public class Student
+    {
+        public int RollNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student(int rn, string fn, string ln)
+        {
+            this.RollNumber = rn;
+            this.FirstName = fn;
+            this.LastName = ln;
+
+        }
+        public static bool operator ==(Student s1, Student s2)
+        {
+            return s1.RollNumber == s2.RollNumber &&
+                s1.FirstName == s2.FirstName &&
+                s1.LastName == s2.LastName;
+        }
+
+
+        public static bool operator !=(Student s1, Student s2)
+        {
+            return s1.RollNumber != s2.RollNumber &&
+                s1.FirstName != s2.FirstName &&
+                s1.LastName != s2.LastName;
+        }
+
+        public static Student operator +(Student s1, Student s2)
+        {
+            int rn = s1.RollNumber + s2.RollNumber;
+            string fn = s1.FirstName + " " + s2.FirstName;
+            string ln = s1.LastName + " " + s2.LastName;
+
+            return new Student(rn, fn, ln);
+
+        }
+
+
+    }
+}
