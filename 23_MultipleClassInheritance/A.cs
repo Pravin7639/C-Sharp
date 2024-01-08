c81bff7250061bd1d744fca38df93a3881f91562
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-08 17:41:49 +0530

    23_Multiple Class Inheritance

---
+++ 23_MultipleClassInheritance/A.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClassInheritance
+{
+    public class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    public class C : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("C");
+        }
+    }
+
+
+    //public class D : A,B   // Multiple Inheritance is not possible 
+    //{
+        
+    //}
+
+
+}
---
+++ 23_MultipleClassInheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClassInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Print();
+
+            A a1 = new B();
+            a1.Print();
+
+            A a2 = new C();
+            a2.Print();
+
+
+            B b = new B();
+            b.Print();
+
+
+            C c = new C();
+            c.Print();
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
