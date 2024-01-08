08a51f1aa7b401bcd4099492a199b9e5808700d5
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-08 17:42:55 +0530

    24_Method Overloading

---
+++ 24_MethodOverLoading/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_MethodOverLoading
+{
+    public class Calculator
+    {
+
+        public void Print()
+        {
+            Console.WriteLine("PrintA() Method Called");
+        }
+
+        public void Print(int a)
+        {
+            Console.WriteLine($"PrintA(int a) = {a} Method called");
+        }
+
+        public void Print(int a, int b)
+        {
+            Console.WriteLine($"Print(int a, int b) = {a} & {b} Method Called");
+        }
+        public void Print(int a, int b, int c)
+        {
+            Console.WriteLine("Print(int a, int b, int c) Method Called");
+        }
+
+        public void Print(string str)
+        {
+            Console.WriteLine("Print(string str) Method Called");
+        }
+
+
+        public void Print(string str, string str1)
+        {
+            Console.WriteLine("Print(string str, string str1) Method Called");
+        }
+
+        public void Print(int a, string str)
+        {
+            Console.WriteLine("Print(int a, string str) Method Called");
+        }
+
+        public void Print( string str, int a)
+        {
+            Console.WriteLine("Print( string str, int a) Method Called");
+        }
+
+        public void Print(ref int a)
+        {
+            Console.WriteLine($"PrintA(ref int a) = {a} Method called");
+        }
+        public void Print(short num)
+        {
+            Console.WriteLine("Print(short num) Called");
+        }
+    }
+}
---
+++ 24_MethodOverLoading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_MethodOverLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            c.Print();
+
+            c.Print(10);
+
+            c.Print(10, 10);
+
+            c.Print(10, 10, 10);
+
+            c.Print("Pawan");
+
+            c.Print("Pravin", "Mane");
+
+            c.Print(45, "Pawan");
+
+            c.Print("Pawan", 45);
+
+            int i = 200;
+            c.Print(ref i);
+
+            c.Print(23);  // it call int method
+
+            short k= 5000;
+            c.Print(k);
+
+
+            Console.ReadLine();
+        }
+    }
+}
