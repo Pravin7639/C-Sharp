2d4674dbf3e47979608349fa648f6f7fefcddb6f
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-09 23:10:14 +0530

    28_Sealed Keyword

---
+++ 28_SealedKeyword/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_SealedKeyword
+{
+    public class Customer
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Customer() Method Called");
+        }
+    }
+
+    public class SilverCustomer : Customer
+    {
+        public sealed override void Print()
+        {
+            Console.WriteLine("Silver Customer() Method Called");
+        }
+    }
+
+    public class GoldCustomer : SilverCustomer
+    {
+        public new void Print()   // can not override this method because of sealed keyword in base class
+        {
+            Console.WriteLine("Gold Silver Customer Print()");
+        }
+    }
+
+
+}
---
+++ 28_SealedKeyword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_SealedKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer c1 = new Customer();
+            c1.Print();
+
+            Customer c2 = new SilverCustomer();
+            c2.Print();
+
+            Customer c3 = new GoldCustomer();
+            c3.Print();
+
+            SilverCustomer sc = new SilverCustomer();
+            sc.Print();
+            
+            GoldCustomer gc = new GoldCustomer();
+            gc.Print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
