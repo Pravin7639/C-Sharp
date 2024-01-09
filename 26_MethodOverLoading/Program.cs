af4a8d84617ff4e0703cb442c9476b04a9a24073
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-09 23:03:56 +0530

    26_Method Overloading

---
+++ 26_MethodOverLoading/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_MethodOverLoadingScenario
+{
+
+    /// METHOD OVERLOADING USING MULTIPLE CLASS
+    /// 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.PrintA();
+
+            B b = new B();
+            b.PrintA(10);
+            b.PrintA();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class A
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("A : PrintA()");
+
+        }
+    }
+
+    public class B : A
+    {
+        public void PrintA(int a)
+        {
+            Console.WriteLine("B : PrintA(int a)");
+        }
+
+    }
+
+
+
+
+}
