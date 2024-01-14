e28d98be6ed5243f40284b8401d3f4c90344e8da
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-14 22:51:05 +0530

    37_Generics

---
+++ 41_Generics/Calculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Generics
+{
+    public class Calculator
+    {
+
+        #region Using Method Overloading
+        //public bool AreEqual(int a, int b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        //public bool AreEqual(string a, string b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        //public bool AreEqual(float a, float b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        #endregion Using Method Overloading
+
+
+        #region Using Object Type
+
+        ////problems with object type
+        ////1... there is no type safety
+        ////2... chances of boxing & unboxing operations
+
+
+
+        //public bool AreEqual(object a, object b)
+        //{
+        //    return a.Equals(b);
+
+        //}
+
+
+
+        #endregion Using Object Type
+
+
+
+        #region using Generic
+
+        public bool AreEqual<T>(T a, T b)
+        {
+
+            return a.Equals(b);
+
+        }
+
+        public void Add<T>(T a, T b)
+        {
+            //  Console.WriteLine(a + b);// cannot be written because datatype is unknown
+
+            //object n1 = a;
+            //object n2 = b;
+
+            //var n1 = a;
+            //var n2 = b;
+
+
+            dynamic n1 = a;
+            dynamic n2 = b;
+
+            Console.WriteLine(n1 + n2);
+        }
+
+
+
+        public TResult MyMethod<Tin, TResult>(Tin a)
+        {
+            dynamic d = null;
+            return d;
+        }
+
+        #endregion using Generic
+
+
+
+
+
+    }
+
+}
---
+++ 41_Generics/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Using Method Overloading
+
+            //Calculator c1 = new Calculator();
+            //bool b1 = c1.AreEqual(5, 5);
+            //Console.WriteLine(b1); //true
+
+            //b1 = c1.AreEqual(5, 6);
+            //Console.WriteLine(b1); //false
+
+            //b1 = c1.AreEqual("ppp", "ppp");
+            //Console.WriteLine(b1);  //true
+
+            //b1 = c1.AreEqual("ppp", "PPP");
+            //Console.WriteLine(b1); //false
+
+            //b1 = c1.AreEqual(5.5f, 2.5f);
+            //Console.WriteLine(b1);// false
+
+            #endregion Using Method Overloading
+
+
+            #region Using Object Type
+
+            //Calculator c1 = new Calculator();
+            //bool b1 = c1.AreEqual(5, 5);
+            //Console.WriteLine(b1); //true
+
+            //b1 = c1.AreEqual(5, 6);
+            //Console.WriteLine(b1); //false
+
+            //b1 = c1.AreEqual("ppp", "ppp");
+            //Console.WriteLine(b1);  //true
+
+            //b1 = c1.AreEqual("ppp", "PPP");
+            //Console.WriteLine(b1); //false
+
+            //b1 = c1.AreEqual(5.5f, 2.5f);
+            //Console.WriteLine(b1);// false
+
+            #endregion Using Object Type
+
+
+
+
+            #region using Generic
+
+            Calculator c1 = new Calculator();
+            bool b1 = c1.AreEqual<int>(5, 5);
+            Console.WriteLine(b1);//true
+
+            b1 = c1.AreEqual<string>("a", "b");
+            Console.WriteLine(b1);//false
+
+            //   b1 = c1.AreEqual("AA", 10.5f);  // not possible
+
+
+            b1 = c1.AreEqual<float>(10.5f, 10.5f);
+            Console.WriteLine(b1);//true
+
+
+            c1.Add<int>(10, 11); //21
+
+
+            c1.Add<string>("a", "b");  //ab
+
+
+            c1.Add<float>(10.5f, 10.5f);  //21
+
+
+
+            /////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////
+
+
+            Student<int, string> s1 = new Student<int, string>();
+            s1.RollNumber = 45;
+            s1.Name = "Pravin";
+            string r = s1.Details();
+            Console.WriteLine(r);
+
+
+
+            Student<string, string> s2 = new Student<string, string>();
+            s2.RollNumber = "45";
+            s2.Name = "PRAVIN";
+            string r1 = s2.Details();
+            Console.WriteLine(r1);
+
+
+
+
+
+
+            #endregion using Generic
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
