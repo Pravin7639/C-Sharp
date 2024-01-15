8abd6c107c8a9659a80ae0042cff5cb82a136a02
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-15 22:56:20 +0530

    40_Multicast Delegate

---
+++ 44_MulticastDelegate/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_MulticastDelegate
+{
+    class Program
+    {
+        public delegate string PrintDelegate();
+        static void Main(string[] args)
+        {
+
+            PrintDelegate pd = PrintA;
+            pd += PrintB;
+            pd += PrintC;
+            string result = pd();
+            Console.WriteLine(result);  /// it will print last added method
+
+            Delegate[] delegates = pd.GetInvocationList();
+            // used for getting step by step proccess of pd
+           // get invocation list
+
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                try
+                {
+                    var result1 = delegates[i].DynamicInvoke();
+                    Console.WriteLine(result1);
+                }
+                catch
+                {
+                    Console.WriteLine("Exception Occured");
+                }
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+        static string PrintA()
+        {
+            return "PrintA()";
+        }
+
+        static string PrintB()
+        {
+            return "PrintB()";
+        }
+        static string PrintC()
+        {
+            return "PrintC()";
+        }
+
+
+    }
+}
