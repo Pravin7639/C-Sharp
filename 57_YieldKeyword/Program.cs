f9521efe8d71dd311de999ba0f9e685fdd20a2f0
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-20 23:59:27 +0530

    53_Yield Keyword

---
+++ 57_YieldKeyword/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57_YieldKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = 
+                new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            foreach (int num in GetEven(numbers))
+            {
+                Console.Write($"{num} ");
+            }
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+        static IEnumerable<int> GetEven(List<int> nums)
+        {
+            // List<int> temp = new List<int>();
+
+            foreach (int num1 in nums)
+            {
+                if (num1 % 2 == 0)
+                {
+                    //    temp.Add(num);
+                    yield return num1;
+                }
+            }
+            // return temp;
+
+
+        }
+
+
+    }
+}
