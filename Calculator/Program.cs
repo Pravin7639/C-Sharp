39ffae1becba43f4c4e0373a0a5a12784acf68de
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-21 23:00:54 +0530

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("WelcomE To Calculator");
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            Console.WriteLine("Please enter a first number : ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a second number : ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter an Opertion : ");
+            Console.WriteLine("+ : Addition");
+            Console.WriteLine("- : Subtraction");
+            Console.WriteLine("* : Multiply");
+            Console.WriteLine("/ : Devide");
+            Console.Write("Enter an Option : ");
+
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Addition {num1} + {num2} = {num1 + num2}");
+                    break;
+
+
+                case "-":
+                    Console.WriteLine($"Addition {num1} - {num2} = {num1 - num2}");
+                    break;
+
+
+                case "*":
+                    Console.WriteLine($"Addition {num1} * {num2} = {num1 * num2}");
+                    break;
+
+
+                case "/":
+                    Console.WriteLine($"Addition {num1} / {num2} = {num1 / num2}");
+                    break;
+
+
+                default:
+                    Console.WriteLine("INVALID OPTION");
+                    break;
+
+
+
+
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
