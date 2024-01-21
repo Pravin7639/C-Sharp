d18154ff6722e6a22cc2e8a395554408c549f4db
Pravin7639 <141111800+Pravin7639@users.noreply.github.com>
2024-01-21 23:00:07 +0530

    55_Lambda Expression

---
+++ 59_LambdaExpression/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _59_LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Using Separate Method
+            //Calculator c1 = new Calculator();
+            //Predicate<int> del1 = c1.Even;
+            //bool b1 = del1(10);
+            //Console.WriteLine(b1);
+
+            //b1 = del1(11);
+            //Console.WriteLine(b1);
+            #endregion Using Separate Method
+
+
+
+            #region Using Anonymous Method
+
+            //Predicate<int> del1 = delegate (int num)
+            //  {
+            //      return num % 2 == 0;
+            //  };
+
+            //Console.WriteLine(del1(10));
+            //Console.WriteLine(del1(11));
+
+
+            #endregion Using Anonymous Method
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+        //static bool Even(int num)
+        //{
+        //    return num % 2 == 0;
+        //}
+
+    }
+
+    public class Calculator
+    {
+        public bool Even(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
